a10a8ff7517f55069a674899c9fe4105270bb97a
gky501 <aaron@aarongilkey.net>
2026-07-14 15:03:15 -0500

    Load camper and trip interface styles

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,8 @@ import './styles.css';
 import './diary-additions.css';
 import './amenities.css';
 import './parks.css';
+import './trip-ui.css';
+import './campers.css';
 import App from './App';
 
 createRoot(document.getElementById('root')!).render(<StrictMode><App /></StrictMode>);
