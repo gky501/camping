0e01e00543f836844acf40b58bd7eb4da3ca0596
gky501 <aaron@aarongilkey.net>
2026-07-14 14:30:06 -0500

    Load parks styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import 'leaflet/dist/leaflet.css';
 import './styles.css';
 import './diary-additions.css';
 import './amenities.css';
+import './parks.css';
 import App from './App';
 
 createRoot(document.getElementById('root')!).render(<StrictMode><App /></StrictMode>);
