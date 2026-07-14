995a1f2d592ca04b4706a298e47c3fe5f674dfe1
gky501 <aaron@aarongilkey.net>
2026-07-14 12:10:29 -0500

    Load amenity card styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import 'leaflet/dist/leaflet.css';
 import './styles.css';
 import './diary-additions.css';
+import './amenities.css';
 import App from './App';
 
 createRoot(document.getElementById('root')!).render(<StrictMode><App /></StrictMode>);
